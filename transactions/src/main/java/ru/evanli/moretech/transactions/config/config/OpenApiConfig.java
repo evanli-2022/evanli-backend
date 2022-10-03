b7dcb6fe86db7b9c7cd639ecb516add12e349d09
shurupov <bolteg86@ya.ru>
2022-10-03 21:43:37 +0400

    Added transactions service

---
+++ transactions/src/main/java/ru/evanli/moretech/transactions/config/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package ru.evanli.moretech.transactions.config.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+    name = OpenApiConfig.BEARER_TOKEN_SECURITY_SCHEME,
+    type = SecuritySchemeType.HTTP,
+    bearerFormat = "JWT",
+    scheme = "bearer"
+)
+public class OpenApiConfig {
+
+    public static final String BEARER_TOKEN_SECURITY_SCHEME = "JWT Token Authentication";
+
+}
---
+++ transactions/src/main/java/ru/evanli/moretech/transactions/config/config/WebSecurityConfig.java
@@ -0,0 +1,60 @@
+package ru.evanli.moretech.transactions.config.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+//    TODO Create filter and uncomment this
+//    private final AuthTokenFilter authenticationJwtTokenFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http)
+        throws Exception {
+
+        http
+            .cors()
+            .and()
+            .csrf().disable().exceptionHandling()
+//            .authenticationEntryPoint(unauthorizedHandler)
+            .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authorizeRequests()
+            .antMatchers("/api/auth/**").permitAll()
+            .antMatchers("/swagger-ui/**", "/transactions/api-docs/**").permitAll()
+            .anyRequest()
+            .authenticated();
+
+//        http.addFilterBefore(authenticationJwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    /*@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }*/
+
+    /*@Bean
+    public AuthTokenFilter authenticationJwtTokenFilter(AuthService authService) {
+        return new AuthTokenFilter(authService, userDetailsService);
+    }*/
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
